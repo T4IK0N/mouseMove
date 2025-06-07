62346259b1ef2d88af8af31363b3ea986e39f525
T4IK0N <gasowskibogumil@gmail.com>
2025-06-07 16:25:41 +0200

    com

--- mouse_over.py
+++ mouse_over.py
@@ -9,7 +9,7 @@ print("program has started")
 while True:
     start_time = time.time()
     while time.time() - start_time < 30:
-        dx = random.randint(-10, 10)
-        dy = random.randint(-10, 10)
+        dx = random.randint(-1, 1)
+        dy = random.randint(-1, 1)
         pyautogui.moveRel(dx, dy, duration=0.1)
-        time.sleep(0.2)
+        time.sleep(60)
